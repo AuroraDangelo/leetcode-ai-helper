c2e38223efdfa252531beccbd341c673a03b5e5a
Princess <anshikapan04@gmail.com>
2026-05-24 18:35:33 +0530

    Add dynamic title scraping

--- content.js
+++ content.js
@@ -1,5 +1,13 @@
-const titleElement=document.querySelector(
-    'a[href="/problems/two-sum/"]'
-);
+const currentURL=window.location.href;
 
-console.log(titleElement);
+const urlParts=currentURL.split("/");
+
+const problemSlug=urlParts[4];
+
+const selector=
+`a[href="/problems/${problemSlug}/"]`;
+
+const titleElement=
+document.querySelector(selector);
+
+console.log(titleElement.textContent);
